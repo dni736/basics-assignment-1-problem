381abd02d11ac7c0def3be26e59c8fa2a20fdb48
dni736 <dima.nikolishvili@gmail.com>
2022-10-28 12:05:30 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var myName = "Demetre";
+var myAge = 26;
+var myAgeIn5 = myAge + 5;
+var myFavNum = Math.random();
+document.getElementById("name").innerText = myName;
+document.getElementById("age").innerText = myAge;
+document.getElementById("ageIn5").innerText = myAgeIn5;
+document.getElementById("favNum").innerText = myFavNum.toFixed(2);
